0da9b81439641a40a2fcfe516150463f98b5989a
zander0071 <88046770+zander0071@users.noreply.github.com>
2022-01-10 20:21:46 -0500

    v2.1

--- scripts.js
+++ scripts.js
@@ -2,3 +2,20 @@ function playSound() {
     var audio = document.getElementById("audio");
     audio.play();
 }
+
+document.addEventListener('DOMContentLoaded',function(event){
+    
+    var dataText = ["C:\\Users\\Alex>"];
+    
+    function typeWriter(text, j, fnCallback) {
+      
+        if (j < (text.length)) {
+            document.querySelector("p1").innerHTML = text.substring(0, j+1);
+            setTimeout(function() {typeWriter(text, j+1, fnCallback);}, Math.floor(Math.random() * 500));
+        }
+
+    }
+    
+    typeWriter(dataText[0], 0, function(){ typeWriter(j+1);});
+
+  });
