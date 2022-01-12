68be8e3ca869860c0cb35f8cb6fb2b4f5deb9370
zander0071 <88046770+zander0071@users.noreply.github.com>
2022-01-11 20:56:14 -0500

    v2.4

--- scripts.js
+++ scripts.js
@@ -5,12 +5,20 @@ function playSound() {
 
 document.addEventListener('DOMContentLoaded',function(event){
     
-    var inputText = ["cd documents\\projects", "tree"];
-    var outputText = ["\n C:\\Users\\Alex\\Documents\\Projects>", "\n bruh"]
+    var inputText = ["cd documents\\projects", "tree","Click one....  *rbt*"];
+    var outputText = [
+        "\n C:\\Users\\Alex\\Documents\\Projects>", 
+        "\n ├───<a href=\"project1.html \" >Project1.html</a>    \
+         \n ├───<a href=\"project2.html \" >Project2.html</a>    \
+         \n ├───<a href=\"project3.html \" >Project3.html</a>    \
+         \n └───<a href=\"resume.html \" >Resume.html</a>    \
+         \n C:\\Users\\Alex\\Documents\\Projects>"
+         
+        ];
 
 
     function printout(el, i, fncallback) {
-        document.getElementById(el).textContent = outputText[i];
+        document.getElementById(el).innerHTML = outputText[i];
         setTimeout(fncallback, 1000);
     }
 
@@ -22,7 +30,7 @@ document.addEventListener('DOMContentLoaded',function(event){
                 function() {
                     typeWriter(el, i, j+1, fncallback);
                 }, 
-                Math.floor(400 * (Math.random() ** 3) + 50));
+                Math.floor(500 * (Math.random() ** 5) + 50));
         }
 
         if (j == inputText[i].length) {
@@ -30,10 +38,37 @@ document.addEventListener('DOMContentLoaded',function(event){
         }
     }
 
+    function speech(el, i, j, fncallback) {
+      
+        if (j <= (inputText[i].length)) {
+            document.getElementById(el).textContent = inputText[i].substring(0, j);
+            setTimeout(
+                function() {
+                    speech(el, i, j+1, fncallback);
+                }, 
+                Math.floor(150));
+        }
+
+        if (j == inputText[i].length) {
+            setTimeout(fncallback, 700);
+        }
+    }
+
+    function appear(fncallback) {
+        setTimeout( () => {
+            document.getElementById('bubble').style.color = 'white'},2000
+        );
+        setTimeout( () => {
+            fncallback();}, 2500
+        );
+    }
+
     typeWriter("input1", 0, 0, () => 
     printout("output1", 0, () =>
     typeWriter("input2", 1, 0, () =>
-    printout("output2", 1))
-    ));
+    printout("output2", 1, 
+    appear( () =>
+    speech("bubbletxt", 2, 0)
+    )))));
 
   });
